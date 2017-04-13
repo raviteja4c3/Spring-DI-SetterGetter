b1e83e196b763736d8083f9075410912af048339
raviteja4c3 <raviteja4c3@gmail.com>
2017-04-13 10:54:30 +0530

    Create SpellChecker.java

---
+++ SpellChecker.java
@@ -0,0 +1,14 @@
+package com.tpoint;
+
+public class SpellChecker {
+	
+	
+	public SpellChecker(){  //This method is not required, just to print msg when object created for this class
+		System.out.println("Inside SpellCheker Constructor ."); 
+	}
+  
+	public void checkSpelling(){
+	      System.out.println("Inside SpellCheker method checkSpelling ..." );
+	}
+
+}
