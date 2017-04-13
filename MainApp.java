d100472adc980a018a6ead9583d37e11bb802c13
raviteja4c3 <raviteja4c3@gmail.com>
2017-04-13 10:51:57 +0530

    Created MainApp.java

---
+++ MainApp.java
@@ -0,0 +1,12 @@
+package com.tpoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+   public static void main(String[] args) {
+      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      TextEditor te = (TextEditor) context.getBean("textEditor");
+       te.spellCheck();  //use for setter, getter based DI
+   }
+}
